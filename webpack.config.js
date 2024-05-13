23494096f06c31ab755779f056e07f8a67f46ff0
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2024-05-13 10:52:25 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   module: {
